2daa362df4973dcb1e47af742fc7b9eaf7442c85
mythLabs <amitbisht744@gmail.com>
2019-08-24 22:02:02 +0530

    render data in SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,17 +1,34 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class SongList extends Component {
+  renderSong() {
+    return this.props.data.songs.map((song, i) => {
+      return <li key={i}>{song.title}</li>;
+    });
+  }
 
-    render() {
-       return( <div>
-            song list
-       </div> )
+  render() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
     }
 
+    return (
+      <div>
+        song list
+        {this.renderSong()}
+      </div>
+    );
+  }
 }
 
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
 
-export default SongList;
-
-
+export default graphql(query)(SongList);
