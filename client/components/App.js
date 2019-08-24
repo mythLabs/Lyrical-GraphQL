78cbc67c74240e0ee28a3e0db8b0782467d01925
mythLabs <amitbisht744@gmail.com>
2019-08-24 22:10:53 +0530

    react-route setup

---
+++ client/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const App = ({ children }) =>{
+    return <div className="container">
+       {children}
+    </div>
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
 
-import SongList from './components/SongList';
+import SongList from "./components/SongList";
+import App from "./components/App"
 
 const client = new ApolloClient({});
 
 const Root = () => {
-  return (<ApolloProvider client={ client }>
-    <div>
-      <div>Lyrical</div>
-      <SongList/>
-    </div>
-    
-  </ApolloProvider>)
+  return <ApolloProvider client={client}>
+    <Router history={hashHistory}>
+        <Route path='/' component={App}>
+          <IndexRoute component={SongList}></IndexRoute>
+        </Route>
+
+
+    </Router>
+  </ApolloProvider>;
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
